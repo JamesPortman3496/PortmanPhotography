9d18163047a7f7d363150910ae372f2a3c195f6d
JamesPortman3496 <James.Portman3496@gmail.com>
2018-06-22 23:45:14 +0100

    update

--- slideshow.js
+++ slideshow.js
@@ -2,55 +2,47 @@ var slideIndex = new Array(2);
 slideIndex[0]=1;
 slideIndex[1]=1;
 
-showSlides(1, 0);  
+showSlides(1, 0);
 showSlides(1, 1);
 
-
-function plusSlides(n, slideshownumber) 
-{
-  slideIndex[slideshownumber] = slideIndex[slideshownumber] + n; 
+function plusSlides(n, slideshownumber) {
+  slideIndex[slideshownumber] = slideIndex[slideshownumber] + n;
   showSlides( slideIndex[slideshownumber], slideshownumber );
 }
 
-function currentSlide(n, slideshownumber) 
-{
+
+function currentSlide(n, slideshownumber) {
   slideIndex[slideshownumber] = n;
   showSlides(slideIndex[slideshownumber], slideshownumber);
 }
 
-function showSlides(n, slideshownumber) 
-{
+function showSlides(n, slideshownumber) {
   var i;
 
-////////////////////////////////////////////////////////////////////////////////////////////
-
-var slideshowname = "slider" + slideshownumber;
+  var slideshowname = "slider" + slideshownumber;
   var slides = document.getElementsByName(slideshowname);
 
-////////////////////////////////////////////////////////////////////////////////////////////
-
-var dotname = "dot" + slideshownumber;
+  var dotname = "dot" + slideshownumber;
   var dots = document.getElementsByName(dotname);
 
-  if (n > slides.length) 
-  {
-      slideIndex[slideshownumber] = 1;
+  if (n > slides.length) {
+    slideIndex[slideshownumber] = 1;
   }
 
-  if (n < 1) 
-  {
-      slideIndex[slideshownumber] = slides.length;
+  if (n < 1) {
+    slideIndex[slideshownumber] = slides.length;
   }
 
-  for (i = 0; i < slides.length; i++) 
-  {
-      slides[i].style.display = "none";
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
 
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
 
   slides[slideIndex[slideshownumber]-1].style.display = "block";
   dots[slideIndex[slideshownumber]-1].className += " active";
-} 
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////
